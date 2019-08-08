d2a2efe9f8cab9eb4005aac2c8285b3f3a902fce
Rasmus Kröger <rasmus.kroger@suoratyo.fi>
2019-08-08 11:01:18 +0300

    Fix firing event on generating content
    -Works on react onChange

--- src/js/content.js
+++ src/js/content.js
@@ -5,10 +5,12 @@ let startDate = '1960-01-01';
 let endDate = '1980-01-01';
 
 chrome.storage.sync.get('settings', function (obj) {
-    const settings = JSON.parse(obj.settings);
-    if(settings.startDate !== undefined && settings.endDate !== undefined) {
-        startDate = settings.startDate;
-        endDate = settings.endDate;
+    if (typeof obj.settings === 'object') {
+        const settings = JSON.parse(obj.settings);
+        if(settings.startDate !== undefined && settings.endDate !== undefined) {
+            startDate = settings.startDate;
+            endDate = settings.endDate;
+        }
     }
 });
 
@@ -31,11 +33,5 @@ function replaceSelectedText(elem, text) {
     elem.value = elem.value.slice(0, start) + text + elem.value.substr(end);
     elem.selectionStart = start + text.length;
     elem.selectionEnd = elem.selectionStart;
-    if ("createEvent" in document) {
-        const evt = document.createEvent("HTMLEvents");
-        evt.initEvent("change", false, true);
-        elem.dispatchEvent(evt);
-    }
-    else
-        elem.fireEvent("onchange");
+    elem.dispatchEvent(new Event('input', { bubbles: true }));
 }
